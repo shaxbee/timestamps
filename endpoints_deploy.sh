dc13ffaeabc69b2d366efb3bc547da592792d83d
merishlalla <lllmer001@myuct.ac.za>
2022-05-19 03:22:25 +0200

    Last clean up

--- endpoints_deploy.sh
+++ endpoints_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#sleep 10 #Sleep to ensure port forwarding gets set up as expected
-
 read -r deployed_time < deploy.txt
 
 curl -d '{"deployed_at":'$deployed_time'}' -H "Content-Type: application/json" -X POST http://0.0.0.0:8000/set_deploy_time
